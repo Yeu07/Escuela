0482700514931e1b1aceeccdff08a873eae39bc1
Yeu <yeumensilva9@gmail.com>
2026-03-03 15:53:17 -0300

    Create Routes

---
+++ src/controllers/estudiantesController.ts
@@ -0,0 +1,61 @@
+import type { Request, Response } from "express";
+
+class EstudiantesControllers {
+    constructor(){}
+
+    consultar(req:Request,res:Response) {
+        try {
+           
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).send(error.message)
+            } 
+        }
+    }
+
+    consultarDetalle(req:Request,res:Response){
+        const {id} = req.params;
+         try {
+           
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).send(error.message)
+            }
+        }
+    }
+
+    ingresar(req:Request,res:Response){
+        try {
+            const {dni, nombre, apellido, email} = req.body;
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).send(error.message)
+            }
+        }
+    }
+
+    actualizar(req:Request,res:Response){
+        try {
+            const {id} = req.params;
+            const {dni, nombre, apellido, email} = req.body;
+           
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).send(error.message)
+            }
+        }
+    }
+
+    borrar(req:Request,res:Response){
+        try {
+            const {id} = req.params;
+          
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).send(error.message)
+            }
+        }
+    }
+}
+
+export default new EstudiantesControllers();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
-
+import cors from 'cors'
+import morgan from 'morgan';
+import estudiantesRoutes from './routes/estudiantesRoutes.js'
+import profesoresRoutes from './routes/profesoresRoutes.js'
+import cursosRoutes from './routes/cursosRoutes.js'
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use("/estudiantes",estudiantesRoutes)
+app.use('/profesores', profesoresRoutes)
+app.use('/cursos', cursosRoutes)
 
 
 app.listen(3000, () => {
